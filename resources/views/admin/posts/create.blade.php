8208d2984d3f3cc7fe1120d28b1b8414a41b9412
robertvlad <vladrob96@gmail.com>
2023-03-05 21:59:07 +0100

    admin.posts.create aggiunta

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,7 +5,25 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            ciao
+            <div class="py-3">
+                <h2>Aggiungi Post</h2>
+            </div>
+            <div>
+                <form action="{{ route('admin.posts.store')}}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label class="control-label">TITOLO</label>
+                        <input type="text" class="form-control" placeholder="Titolo" id="title" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label">CONTENUTO</label>
+                        <textarea name="content" id="content" cols="30" rows="10" placeholder="Contenuto" class="form-control"></textarea>
+                    </div>
+                    <div class="form-group my-2">
+                        <button type="submit" class="btn btn-success">Salva</button>                        
+                    </div>                    
+                </form>
+            </div>
         </div>
     </div>
 </div>
